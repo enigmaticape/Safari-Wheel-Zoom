fb38e91d6d60f97b88dcf95db4064e28c27a1321
Steve Trewick <steve@enigmaticape.com>
2013-05-31 13:12:37 +0100

    Smother zooming with --webkit-transform, bump version, add icon

--- inject.js
+++ inject.js
@@ -3,22 +3,35 @@ zoomLevel     = 1;
 zoomIncrement = 0.2;
 
 document.addEventListener("mousewheel", function( event ) {
-
-    if( zoomWithWheel ) {
+    
+    
+    if( zoomWithWheel  )  {
+        
+        event.preventDefault();
+        event.stopPropagation();
+        
         if( event.wheelDeltaY > 0  ) {
             zoomLevel += zoomIncrement;
         }
         if( event.wheelDeltaY < 0 ) {
             zoomLevel -= zoomIncrement;
         }
-        document.body.style.zoom = zoomLevel;
-        return false;
+        
+        // CSS transform works much less jankily than zoom
+        document.body.style.webkitTransform = "scale(" + zoomLevel + ")";
+        document.body.style.webkitTransformOrigin="0 0";
+        
     }
 }, true );
 
 document.addEventListener("click", function( event ) {
+    
     if(event.button == 1) {
+      
       event.preventDefault();
+      event.stopPropagation();
+      
       zoomWithWheel = !zoomWithWheel;
+
     }
 }, true);
